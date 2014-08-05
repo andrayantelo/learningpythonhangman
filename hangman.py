0c42d90aad8d0d7198fed0a7d5085da117cc5e76
andrayantelo <andrayantelo@gmail.com>
2014-08-05 17:49:36 -0500

    fixed the indentations

--- hangman.py
+++ hangman.py
@@ -15,32 +15,32 @@ def start():
 	
 
 def randomwordchoice():
-	# using a small word list for now
-	wordlist = 'Apple Watermelon Pineapple Papaya Strawberry Blueberry Fig Durian'.split()
-	lowerwordlist = [letter.lower() for letter in wordlist] # make everything lower case
+    # using a small word list for now
+    wordlist = 'Apple Watermelon Pineapple Papaya Strawberry Blueberry Fig Durian'.split()
+    lowerwordlist = [letter.lower() for letter in wordlist] # make everything lower case
 	
-	secretword = random.choice(lowerwordlist)
+    secretword = random.choice(lowerwordlist)
 	#printing out the secretword just to see if spaces match update
 	
 	
-	print "_ "*len(secretword)
+    print "_ "*len(secretword)
 	
-	guesses = ''
+    guesses = ''
 	
-	while True: #ask about formatting
-		guess = str(raw_input("\nGuess a letter!\n> "))
-		guess = guess.lower()
+    while True: #ask about formatting
+        guess = str(raw_input("\nGuess a letter!\n> "))
+        guess = guess.lower()
 		
 		
-		guesses += guess
+        guesses += guess
 		
 		# trying out a list for the incorrectguesses to use later when programming the hangman appearance
-		incorrectguesses = []
-		for letter in guesses:
-			if letter not in secretword:
-				incorrectguesses.append(letter)
-			else:
-				pass
+        incorrectguesses = []
+        for letter in guesses:
+            if letter not in secretword:
+                incorrectguesses.append(letter)
+            else:
+                pass
           
         # keeping track of correct guesses
         correctguesses = []
@@ -51,33 +51,33 @@ def randomwordchoice():
                 pass
 		
 		# I want it to appear like I'm not just printing the same things over and over	
-		print "\n"*50
+        print "\n"*50
 		
-		for letter in secretword:
-			if letter in guesses:
-				print letter,
-			else:
-				print '_',
-		print "\n These are the guesses you have made so far:\n", guesses
+        for letter in secretword:
+            if letter in guesses:
+                print letter,
+            else:
+                print '_',
+        print "\n These are the guesses you have made so far:\n", guesses
 		# i want to then print a body part but i want the body parts to be printed at the right time
 		
-		if len(incorrectguesses) == 0:
-			print noman
-		if len(incorrectguesses) == 1:
-			print head
-		if len(incorrectguesses) == 2:
-			print onearm
-		if len(incorrectguesses) == 3:
-			print twoarms
-		if len(incorrectguesses) == 4:
-			print torso
-		if len(incorrectguesses) == 5:
-			print torso2
-		if len(incorrectguesses) == 6:
-			print oneleg
-		if len(incorrectguesses) == 7:
-			print hungman
-			print "GAME OVER YOU'RE DEAD"
+        if len(incorrectguesses) == 0:
+            print noman
+        if len(incorrectguesses) == 1:
+            print head
+        if len(incorrectguesses) == 2:
+            print onearm
+        if len(incorrectguesses) == 3:
+            print twoarms
+        if len(incorrectguesses) == 4:
+            print torso
+        if len(incorrectguesses) == 5:
+            print torso2
+        if len(incorrectguesses) == 6:
+            print oneleg
+        if len(incorrectguesses) == 7:
+            print hungman
+            print "GAME OVER YOU'RE DEAD"
             sys.exit()
         if len(correctguesses) == len(secretword):
             print "YOU WIN! Play again?"
