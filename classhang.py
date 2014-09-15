d96be3966702e9433c6e1567f582698081c6fbd6
Andrea Anaya <andrayantelo@gmail.com>
2014-09-15 14:41:17 -0500

    trying to figure out the correct_guesses and incorrect_guesses

--- classhang.py
+++ classhang.py
@@ -35,24 +35,7 @@ def clear_screen(self):
 #clear screen variable (how many blank lines to print)
     print "\n"* self.screen_size
         
-def intro(self): 
-    self.cool_print(self.intro_text)
-            
-    raw_input("\n\nPRESS ENTER TO CONTINUE.\n> ") 
-        
-    self.clear_screen()
-    print king
-    time.sleep(1)
-    self.clear_screen()
-    print king_speaks
-    time.sleep(1)
-    self.clear_screen()
-    print king
-    time.sleep(1) 
-    self.clear_screen()
-        
-        
-   
+
 HANGMAN_WIN = 'HANGMAN_WIN'
 HANGMAN_LOSE = 'HANGMAN_LOSE'
 HANGMAN_ALREADYGUESSED = 'ALREADY_GUESSED'
@@ -78,16 +61,15 @@ class Hangman(object):
         self.guesses = []
         self.incorrect_guesses = []
         self.correct_guesses = []
-        
 
-   
+        
     def random_word_choice(self):
         #self.secret_word = random.choice(self.wordlist)
         self.secret_word = list(self.secret_word)
         for i, letter in enumerate(self.secret_word):
             if letter not in self.guesses:
                 self.secret_word[i] = "_"
-        print ' '.join(self.secret_word)
+        print ' '.join(self.secret_word)    
         return ' '.join(self.secret_word)
         
     def validate_guess(self, guess):
@@ -107,108 +89,11 @@ class Hangman(object):
     
             if letter in self.secret_word:
                 self.correct_guesses.append(letter)
-        return {"correct": self.correct_guesses, 
-                 "incorrect": self.incorrect_guesses}
         
         
         
-    def game(self):
-        self.intro()
-        self.random_word_choice()
         
 
-        while True: #ask about formatting
-            self.incorrect_guesses = []
-            self.correct_guesses = []
-        
-            self.guess = str(raw_input("\nGuess a letter!\n> "))
-            self.guess = self.guess.lower()
-            allowed_letters = 'abcdefghijklmnopqrstuvwxyz'
-        
-            if len(self.guess) != 1:
-                self.clear_screen()
-                print "Type in a single letter."
-                self.secretword_spaces(self.secret_word)
-                print "\n These are the guesses you have made so far:\n", self.guesses
-                self.hangman_ascii()
-                
-                continue
-            if self.guess in self.guesses:
-                self.clear_screen()
-                print "You already guessed that."
-                self.secretword_spaces(self.secret_word)
-                print "\n These are the guesses you have made so far:\n", self.guesses
-                self.hangman_ascii()
-               
-                continue
-            if self.guess not in allowed_letters:
-                self.clear_screen()
-                print "Please type in a single letter from the English alphabet."
-                self.secretword_spaces(self.secret_word)
-                print "\n These are the guesses you have made so far:\n", self.guesses
-                self.hangman_ascii()
-                
-                continue
-    
- 
-            self.clear_screen()
-            
-            self.guesses += self.guess
-            
-            self.incorrectguesses(self.guesses)
-            print self.incorrect_guesses
-            self.correctguesses(self.guesses)
-            print self.correct_guesses
-            
-            self.secretword_spaces(self.secret_word)
-            print "\n These are the guesses you have made so far:\n", self.guesses
-            self.hangman_ascii()
-            
-            
-                
-    def hangman_ascii(self):
-        if len(self.incorrect_guesses) == 0:
-            print noman
-        if len(self.incorrect_guesses) == 1:
-            print head
-        if len(self.incorrect_guesses) == 2:
-            print onearm
-        if len(self.incorrect_guesses) == 3:
-            print twoarms
-        if len(self.incorrect_guesses) == 4:
-            print torso
-        if len(self.incorrect_guesses) == 5:
-            print torso2
-        if len(self.incorrect_guesses) == 6:
-            print oneleg
-        if len(self.incorrect_guesses) == 7:
-            print hungman
-            time.sleep(5)
-            print laughing_king, '\n\n\n\n'
-            print "The secret word was: %s.\n\n" % self.secret_word
-            print "\n\nGAME OVER YOU'RE DEAD"
-            ask_user_play()
-        
-
-def ask_user_play():
-    while True:
-        print "\n Would you like to play again?"
-        response = str(raw_input("y or n?\n> "))
-        response = response.lower()
-        if response == 'y':
-            mygame = Hangman()
-            mygame.game()
-        if response == 'n':
-            exit()
-        else:
-            continue
-
-#response = "y"
-#while True:
-    
-#    if response == "y":        
-        
- #       ask_user_play()
 
 mygame = Hangman()
 mygame.random_word_choice()
@@ -217,3 +102,4 @@ print mygame.validate_guess(mygame.guess)
 print mygame.guesses
 mygame.guess_status()
 print mygame.correct_guesses
+print mygame.incorrect_guesses
